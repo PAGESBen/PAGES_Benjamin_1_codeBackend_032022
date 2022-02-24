d5505ca03ab4f4531dc32cc1d45424eda00ea9c1
Benjamin PAGES <bpages@MLHR.com>
2022-02-24 15:23:27 +0100

    correction controller user login

--- controllers/user.js
+++ controllers/user.js
@@ -47,26 +47,3 @@ exports.userLogin = (req, res, next) => {
             .catch(error => res.status(500).json({error}));
 
 }
-//     User.findOne({ email : req.body.email })
-//         .then( user => {
-//             if (!user) {
-//                 return res.status(401).json({ message : 'utilisateur introuvable !'})
-//             }
-//             bcrypt.compare(req.body.password, user.password)
-//                 .then(valid => {
-//                     if (!valid){
-//                         return res.status(401).json({ message : 'mot de passe incorrect !'})
-//                     }
-//                     res.status(200).json({
-//                         userId : user._id,
-//                         token : jwt.sign(
-//                             { userId : user._id }, 
-//                             process.env.TOKEN_KEY,
-//                             { expiresIn : '24h' }
-//                         )
-//                     });
-//                 })
-//                 .catch(error => res.status(500).json({error}));
-//         })
-//         .catch(error => res.status(500).json({error}));
-// }
