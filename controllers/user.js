12718765ff61e16f893cfdcceefd62b0546db6a2
Benjamin PAGES <bpages@MLHR.com>
2022-02-26 13:41:54 +0100

    creation des routes getOneUser et modifyOneUser

--- controllers/user.js
+++ controllers/user.js
@@ -2,6 +2,11 @@ const db = require('../config/db');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+//genration de l'url de l'image pour multer
+const generateProfilImgUrl = (req) => {
+    return `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+}
+
 //création d'un utilisateur
 exports.userSignup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
@@ -25,12 +30,12 @@ exports.userLogin = (req, res, next) => {
     )
         .then(([rows]) => { // jeremy! :  fields pas obligatoire !!!
             if (!rows[0]) {
-                return res.status(401).json({ message : 'utilisateur introuvable !!!'})
+                return res.status(404).json({ message : 'utilisateur introuvable !!!' })
             }
             bcrypt.compare(req.body.password, rows[0].password)
                 .then(valid => {
                     if (!valid){
-                        return res.status(401).json({ message : 'mot de passe incorrect !'})
+                        return res.status(401).json({ message : 'mot de passe incorrect !' })
                     }
                     res.status(200).json({
                         userId : rows[0].id,
@@ -47,15 +52,62 @@ exports.userLogin = (req, res, next) => {
 
 }
 
-exports.userGet = (req, res, next) => {
+//récuperation du profil d'un user
+exports.getOneUser = (req, res, next) => {
     db.promise().query(
-        'SELECT * FROM `user` WHERE `email` =?', 
-        [req.body.email]
+        'SELECT `id`, `firstname`, `lastname`, `email`, `imageURL`, `position`  FROM `user` WHERE `id` =?', 
+        [req.params.id] //l'id de l'utilisateur est celui de l'URL
         )
-        .then (([rows, fields]) => {
-            if(!rows[0].id) {
-                return res.status(401)
+        .then(([rows]) => {
+            if(!rows[0]) {
+                return res.status(404).json({ message : 'utilisateur introuvable !!!' })
+            } else {
+                return res.status(200).json(rows[0])
             }
-            
-        }) 
+        })
+        .catch((error) => res.status(500).json({error}));
+}
+
+//Modification de la fiche d'un utilisateur
+exports.modifyOneUser = (req, res, next) => {
+
+    db.promise().query(
+        'SELECT `id`, `imageURL` FROM `user` WHERE `id` = ? ', 
+        [req.body.userId]
+    )
+    .then(([rows]) => {
+
+        // si l'utilisateur n'est pas trouvé
+        if(!rows[0]) {
+            return res.status(404).json({
+                error : new Error('Utilisateur introuvable !!!').message
+            });
+        }
+
+        //si l'utilisateur ne correspond pas au demandeur
+        if(rows[0].id !== req.auth.userId) {
+            return res.status(403).json({
+                error : new Error('Vous ne disposez pas des droits nécéssaires pour faire cette action !!!').message
+            });
+        }
+
+        //si la requete contient une nouvelle image
+        const userObject = req.file ?
+        {
+            ...JSON.parse(req.body.user), 
+            newImg : generateImgUrl(req)
+        } : {
+            ...req.body, 
+            newImg : rows[0].imageURL
+        }
+
+        //on va modifier l'utilisateur
+        db.promise().query(
+            'UPDATE `user` SET `firstname` = ?, `lastname` = ?, `email` = ?, `position` = ?, `imageURL` = ? WHERE `id = ?`', 
+            [...userObject]
+            )
+            .then(() => res.status(200).json({ message : 'Profil utilisateur mis à jour avec succès !' }))
+            .catch(error => res.status(400).json({error}));
+    })
+    .catch(error => res.status(500).json({error}));
 }
--- routes/user.js
+++ routes/user.js
@@ -1,28 +1,11 @@
 const express = require('express');
-const res = require('express/lib/response'); // jeremy! Ne sert à rien !
 const router = express.Router();
 const userCtrl = require('../controllers/user');
-const db = require('../config/db');
-const { getMaxListeners } = require('../config/db');
+const auth = require('../middleware/auth');
 
 router.post('/auth/signup', userCtrl.userSignup /*fonction signup*/);
 router.post('/auth/login', userCtrl.userLogin /*fonction login*/);
-// router.get('/auth/getUser', userCtrl.userGet /*récupération du profil*/)
-// router.put('/auth/modify', userCtrl.userModify /*modification du profil*/);
-
-/** Route en attente pour faire les autres routes
- * Peut servir aux administrateurs pour donner des droits
- */
-
-router.get('/user', (req, res, next) => {
-    //Requete à placer dans le controler
-    db.promise().query(
-        "SELECT * FROM `user` WHERE `email` = ?", 
-        ['amandine.mathieu13@gmail.co']
-    )
-        .then(([results, fields]) => console.log(results))
-        .catch(() => res.status(401))
-
-})
+router.get('/user/:id', auth, userCtrl.getOneUser /*récupération d'un profil*/)
+router.put('/user/:id', auth, userCtrl.modifyOneUser /*modification du profil*/);
 
 module.exports = router;
