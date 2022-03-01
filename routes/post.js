4d57a3c5914ffa017de5f0afa9d6909de116bb96
Benjamin PAGES <bpages@MLHR.com>
2022-03-01 23:49:50 +0100

    ajout route post delete

--- routes/post.js
+++ routes/post.js
@@ -8,6 +8,7 @@ router.get('/', auth, postCtrl.getAllPosts /**Récupération de tous les posts*/
 router.get('/:id', auth, postCtrl.getOnePost /**récuperation d'un post */)
 router.post('/', auth, multer, postCtrl.postOnePost /**Ajout d'un post */);
 router.put('/:id', auth, multer, postCtrl.modifyOnePost /**Modification d'un post */);
+router.delete('/:id', auth, postCtrl.deleteOnePost /**Suppression d'un post */);
 
 
 module.exports = router;
